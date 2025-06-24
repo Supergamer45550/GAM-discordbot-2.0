695eb8b377719da99f553c023a42033f675f6001
Supergamer45550 <gamespcandmobile93@gmail.com>
2025-06-24 02:57:52 +0200

    added main.py
    not functional but at least a start

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+
+intents = discord.Intents.default()
+intents.messages = True
+intents.message_content = True
+intents.members = True
+intents.bans = True
+intents.guilds = True
+intents.guild_messages = True
+
+
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+client = discord.Bot(command_prefix='', intents=intents)
+
+
+
+bot.run("")
